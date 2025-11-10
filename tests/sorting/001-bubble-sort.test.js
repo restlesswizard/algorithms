d52821c566152d7a58b1fd765733a0e4dcac2149
restlesswizard <erxme@yandex.ru>
2025-11-11 02:44:53 +0500

    feat: add tests for sortBubble() and sortBubbleOpt()

--- tests/sorting/001-bubble-sort.test.js
+++ tests/sorting/001-bubble-sort.test.js
@@ -1,1 +1,26 @@
 //test
+
+import { performance } from 'perf_hooks';
+import { sortBubble, sortBubbleOpt } from "../../src/algorithms/sorting/001-bubble-sort"
+
+const INIT = [9, 4, 2, 3, 10, 7, 31, 14, 6];
+const EXPECTED = [2, 3, 4, 6, 7, 9, 10, 14, 31];
+
+test('bubbleSort - return sorted array from less to biggest', () => {
+    const array = INIT.slice();  // prevent mutation on test
+    const start = performance.now();
+    const result = sortBubble(array);
+    const time = performance.now() - start;
+    console.log(`bubbleSort took ${time.toFixed(3)} ms`);
+    expect(result).toEqual(EXPECTED);
+});
+
+
+test('bubbleSortOpt - return sorted array from less to biggest, but should be faster than first', () => {
+  const array = INIT.slice();  // prevent mutation on test
+  const start = performance.now();
+  const result = sortBubbleOpt(array);
+  const time = performance.now() - start;
+  console.log(`bubbleSortOpt took ${time.toFixed(3)} ms`);
+  expect(result).toEqual(EXPECTED);
+});
